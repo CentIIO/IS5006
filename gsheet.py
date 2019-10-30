92b886b295b757559eabc74b17fdb76ea9a37a42
JackCChen2017 <jackchenxing@live.com>
2019-10-30 22:07:31 +0800

    add gsheet.py and credential file.

---
+++ gsheet.py
@@ -0,0 +1,43 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas
+import os
+
+def update_google_sheet_csv(seller):
+    # ## Initialise the client
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    credential_file = 'Group4-GoogleSheet-Credential.json'
+    client = gspread.authorize(ServiceAccountCredentials.from_json_keyfile_name(credential_file, scope))
+
+    formatted_seller_name = seller.name.replace(" ","_")
+    sheet_file_name = "IS5006_Group4_HW2_" + formatted_seller_name
+    print("sheet_file_name:",sheet_file_name)
+
+    # ### Create a sheet. if sheet is already created, comment out it
+    sheet = client.create(sheet_file_name)
+
+    # ### Open a spreadsheet file
+    sheet = client.open(sheet_file_name)
+
+    # ### Sharing a sheet with yourself to look at it and edit it.
+    # update your email address that has google credentials
+    # if the sheet is already shared, comment out it.
+    sheet.share('a0197117y.receiver@gmail.com', perm_type='user', role='writer')
+
+    # ### Selecting a worksheet
+    # worksheet = sheet.worksheet("Sheet1")
+
+    # clear the worksheet
+    # worksheet.clear()
+
+    # export data to csv and import to populate google sheet
+    csv_path = os.path.join("log",formatted_seller_name + '_Data.csv')
+    pandas.DataFrame({'Sales': seller.sales_history,
+                      'Revenue': seller.revenue_history,
+                      'Profit': seller.profit_history,
+                      'Expense': seller.expense_history[1:],
+                      'Sentiment': seller.sentiment_history}).to_csv(csv_path, mode='w', index=False)
+
+    content = open(csv_path, 'r').read()
+    client.import_csv(sheet.id, content)
