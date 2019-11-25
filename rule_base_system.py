b4894995b42de02f6820b706db3d29a4912953bb
JackCChen2017 <jackchenxing@live.com>
2019-11-25 21:09:22 +0800

    replace type with customer_type to avoid confusing with builtin name.

--- rule_base_system.py
+++ rule_base_system.py
@@ -1,16 +1,16 @@
 
-def rbs_get_customer_attributes(type):
-    if type >= 0.8:
+def rbs_get_customer_attributes(customer_type):
+    if customer_type >= 0.8:
         quality_tolerance = 0.8
         price_tolerance = 350
         sentiment_tolerance = 0.8
         ratio = 0.6
-    elif type >= 0.6:
+    elif customer_type >= 0.6:
         quality_tolerance = 0.7
         price_tolerance = 300
         sentiment_tolerance = 0.6
         ratio = 0.08
-    elif type >= 0.4:
+    elif customer_type >= 0.4:
         quality_tolerance = 0.5
         price_tolerance = 200
         sentiment_tolerance = 0.4
