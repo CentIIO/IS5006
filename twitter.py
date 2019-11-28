59274d28e4f84266eed257804a7c1e1839b26e12
amansinghania114 <aman.singhania@yahoo.com>
2019-11-29 00:11:46 +0800

    CODE COMMENTED

--- twitter.py
+++ twitter.py
@@ -1,22 +1,28 @@
+# ALL IMPORTS REQUIRED BY THE CLASS  
 from collections import defaultdict
 import logging
 from copy import copy
 
+# TWITTER CLASS 
 class Twitter(object):
-    # dictionary to store tweets
+    # DICTIONARIES TO STORE DATA
     feed = defaultdict(list)
     feed1 = defaultdict(list)
-    # Called by the user to tweet something
+    
+    # FUNCTION TO POST A TWEET. IT IS CALLED BY THE CUSTOMER WHEN HE DECIDES TO TWEET ABOUT ONE OF HIS OWNED PRODUCTS
     @staticmethod
     def post(user, product, tweet):
         Twitter.feed[product.name].append((user.name, tweet))
         #Twitter.feed1[product.name].append((user.name, tweet))
-        twitter_feed_items = copy(Twitter.feed) # this is to avoid RuntimeError: dictionary changed size during iteration
+        
+        # THIS IS TO AVOID RUNTIMEERROR: DICTIONARY CHANGED SIZE DURING ITERATION
+        twitter_feed_items = copy(Twitter.feed) 
         for i in twitter_feed_items:
             s=str(i)
             if not (s.startswith("(<p")):
                 logging.info ('[Twitter]:History of Twitter post %s ',s)
-    # returns the latest tweet about a product.
+                
+    # FUNCTION THAT RETURNS THE LATEST TWEET ABOUT A PRODUCT.
     @staticmethod
     def get_latest_tweets(product_name, n):
         return [tweet for user, tweet in Twitter.feed[product_name][-n:]]
