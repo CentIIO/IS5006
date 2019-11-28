fcf08e3d445e142e3e470008680d677e725e1276
amansinghania114 <aman.singhania@yahoo.com>
2019-11-29 01:41:13 +0800

    FINAL EDITS

--- google_ads.py
+++ google_ads.py
@@ -1,3 +1,4 @@
+# ALL IMPORTS NEEDED FOR THIS CLASS 
 import random
 from collections import defaultdict
 from threading import Lock
@@ -7,41 +8,43 @@ import json
 
 random.seed(seed)
 
-
+# CLASS GOOGLEADS
 class GoogleAds(object):
-    # Define the types of adverts available
+    # THE DIFFERENT TYPES OF ADVERTS AVAILABLE
     ADVERT_BASIC = 'BASIC'
     ADVERT_TARGETED = 'TARGETED'
     ADVERT_BUNDLED = 'BUNDLED'
 
-    # Define advert's price
+    #DICTIONARY WITH KEY AS ADVERT TYPE AND VALUE AS PRICE OF THE PARTICULAR ADVERT TYPE
     advert_price = {
         ADVERT_BASIC: 5,
-        ADVERT_TARGETED: 10,
-        ADVERT_BUNDLED: 7
+        ADVERT_TARGETED: 10
     }
 
-    # Google's internal database
+    #GOOGLE'S INTERNAL DATABASE COMPRISING OF A LIST TO STORE USERS, AND TWO DICTIONARIES TO STORE EXPENSES AND 
+    #TRACK USER PURCHASE HISTORY RESPECTIVELY
     users = []
     expenses = defaultdict(list)
     purchase_history = defaultdict(list)
 
     lock = Lock()
 
-    # post an advert about the product
+    # FUNCTION TO POST ADVERTISEMENT FOR A PRODUCT PERTAINING TO A SELLER BASED ON AD_TYPE AND SCALE SET
     @staticmethod
     def post_advertisement(seller, product, advert_type, scale):
-        # scale of adverts should not be more than number of users
+        
+        # SCALE OF ADVERTS SHOULD NOT BE MORE THAN NUMBER OF USERS
         scale = min(scale, len(GoogleAds.users))
         GoogleAds.lock.acquire()
 
-        # if advert_type is basic, choose any set of customers
+        # CHECK FOR ADVERT TYPE AND IF ADVERT_TYPE IS THEN CHOOSE ANY SET OF CUSTOMERS
         if advert_type == GoogleAds.ADVERT_BASIC:
             users = random.choices(GoogleAds.users, k=scale)
             test = ', '.join(x.name for x in users)
             logging.info('[GoogleAds]: Google pushed the %s Ad for product %s to users %s ', advert_type, product.name,
                          test)
-        # if advert_type is targeted, choose user's who were not shown the same advert in previous tick
+            
+        # CHECK IF ADVERT_TYPE IS TARGETED, THEN CHOOSE USERS WHO DON’T OWN THE PARTICULAR PRODUCT 
         elif advert_type == GoogleAds.ADVERT_TARGETED:
             new_users = list(set(GoogleAds.users) - set(GoogleAds.purchase_history[product]))
             users = random.choices(new_users, k=scale)
@@ -53,21 +56,22 @@ class GoogleAds(object):
             GoogleAds.lock.release()
             return
 
-        # publish the advert to selected user
+        # PUBLISH THE ADVERT TO SELECTED USERS
         scale = len(users)
         for user in users:
             user.view_advert(product)
 
-        # update the bill into seller's account
+        # UPDATE THE BILL INTO SELLER'S ACCOUNT AND ADD TO THE EXPENSE DICTIONARY
         bill = scale * GoogleAds.advert_price[advert_type]
         GoogleAds.expenses[seller.name].append(bill)
         data = json.loads(json.dumps(GoogleAds.expenses))
         logging.info('[GoogleAds]: Google billed the Seller %s  ', data)
         GoogleAds.lock.release()
 
-        # return the bill amount to the seller
+        # RETURN THE BILL AMOUNT TO THE SELLER
         return bill
 
+    # FUNCTION TO REGISTER USERS TO THE LIST OF USERS MAINTAINED BY GOOGLE
     @staticmethod
     def register_user(user):
         GoogleAds.lock.acquire()
@@ -77,6 +81,7 @@ class GoogleAds(object):
             user.name, user.quality_tolerance, user.price_tolerance, user.sentiment_tolerance)
         GoogleAds.lock.release()
 
+    # FUNCTION TO TRACK USER PURCHASE HISTORY
     @staticmethod
     def track_user_purchase(user, product):
         GoogleAds.lock.acquire()
@@ -87,6 +92,7 @@ class GoogleAds(object):
                 logging.info('[GoogleAds]: Google purchase history %s ', s)
         GoogleAds.lock.release()
 
+    # FUNCTION TO COMPUTE USER COVERAGE
     @staticmethod
     def user_coverage(product):
         return len(set(GoogleAds.purchase_history[product])) / len(GoogleAds.users)
