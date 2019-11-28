b39b5852025275d61e789ae97f7c4e7f077c978f
CentIIO <a0118085@u.nus.edu>
2019-11-28 23:37:43 +0800

    Updating Logs
    Signed-off-by: USRER121 <MOHAMEDHAROON26@GMAIL.COM>

--- market.py
+++ market.py
@@ -1,16 +1,17 @@
-# add product accessory support
 from threading import Lock
 from google_ads import GoogleAds
 import logging
-from tinydb import TinyDB as db
-from tinydb import TinyDB as dbquery
 
+# MARKET CLASS TO SIMULATE THE OPERATIONS OF THE MARKET
 class Market(object):
+
+    # CATALOGUE IS A DICTIONARY WITH THE KEY AS THE MAIN PRODUCT AND VALUE AS THE SELLER SELLING IT
     catalogue = {}
+    # INVENTORY IS A DICTIONARY WITH THE KEY AS MAIN PRODUCT AND VALUE BEING THE AMOUNT OF PRODUCT
     inventory = {}
     lock = Lock()
 
-    # initialise the seller catalogue
+    # INITIALIZING THE SELLER CATALOGUE BY REGISTERING THE SELLER IN THE CATALOGUE AND ASSIGNING THE INVENTORY WITH THE PRODUCT AMOUNT
     @staticmethod
     def register_seller(seller, product):
         Market.lock.acquire()
@@ -20,29 +21,34 @@ class Market(object):
         Market.inventory[product] = 0
         Market.lock.release()
 
-    # when a user buys a product, increment the seller's saleshistory
+    # BUY METHOD
     @staticmethod
     def buy(buyer, product):
-        # get the seller for product from catalogue
+        # GET SELLER INFO FOR THE PARTICULAR PRODUCT USING THE CATALOGUE
         seller = Market.catalogue[product]
 
-        # call seller's sold function
+        # CALLING THE SOLD FUNCTION AT THE TIME OF PRODUCT SALE
         seller.sold(product)
         logging.info("[Market]:Notify Seller %s about the sale of product %s ",seller.name,product.name)
-        # deduct price from user's balance
+
+        # DEDUCTING THE PRICE OF THE PRODUCT BOUGHT FROM THE CUSTOMER'S WALLET
         buyer.deduct(product.price)
         logging.info("[Market]:Get the amount $ %s from buyer %s for product %s ",product.price,buyer.name,product.name)
         Market.inventory[product] -= 1
-        # track user
+
+        # TRACKING USERS THAT BOUGHT PARTICULAR PRODUCTS
         GoogleAds.track_user_purchase(buyer, product)
         logging.info("[Market]:Add buyer %s purchased the product %s to google Add track history",buyer.name,product.name)
 
+
+    # THE UPDATE INVENTORY FUNCTION THAT IS USED TO UPDATE THE AMOUNT OF PRODUCT PER MONTH
     @staticmethod
     def update_inventory(product, amount):
         Market.lock.acquire()
         Market.inventory[product] += amount
         Market.lock.release()
 
+    # THE GET INVENTORY FUNCTION WHICH IS USED TO GET INFO ABOUT AMOUNT OF PRODUCT REMAINING
     @staticmethod
     def get_inventory(product):
         return Market.inventory[product]
